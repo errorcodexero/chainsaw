6bf39f74ce21e87425f9955f282aecafc203c417
macovskym <macovsky.m@gmail.com>
2017-02-15 20:14:34 -0800

    Update OI with new control scheme

--- input/panel.h
+++ input/panel.h
@@ -7,31 +7,33 @@
 struct Panel{
 	bool in_use;
 	//*Buttons:
-	bool learn;
-	bool collect;
-	bool shoot_prep;
+	bool ball_collect;
+	bool loading_indicator;
 	bool shoot;
-	bool gear_collect;
 	bool gear_prep_collect;
 	bool gear_prep_score;
+	bool gear_collect;
 	bool gear_score;
 	bool climb;
+	bool learn;
 	//2 position swicthes:
 	//3 position switches:
-	enum class Gear_lifter{UP,DOWN,AUTO};
-	Gear_lifter gear_lifter;
-	enum class Gear_grabber{OPEN,CLOSE,AUTO};
-	Gear_grabber gear_grabber;
-	enum class Shooter{ON,OFF,REVERSE};
+	enum class Ball_arm{UP,DOWN,AUTO};
+	Ball_arm ball_arm;
+	enum class Gear_grasper{OPEN,CLOSED,AUTO};
+	Gear_grasper gear_grasper;
+	enum class Gear_collector{UP,DOWN,AUTO};
+	Gear_collector gear_collector;
+	enum class Shooter{ENABLED,DISABLED,AUTO};
 	Shooter shooter;
-	enum class Arm{STOW,LOW,AUTO};
-	Arm arm;
-	enum class Intake_direction{IN,OUT,AUTO};
-	Intake_direction intake_direction;
-	enum class Intake_control{ON,OFF,AUTO};//for intake//TODO
-	Intake_control intake_control; 
-	enum class Lift{UP,DOWN,AUTO};
-	Lift lift;
+	enum class Ball_intake{OUT,IN,AUTO};
+	Ball_intake ball_intake;
+	enum class Ball_lift{OUT,IN,AUTO};
+	Ball_lift ball_lift;
+	enum class Shooter_belt{ENABLED,DISABLED,AUTO};
+	Shooter_belt shooter_belt;
+	enum class Ball_collector{DISABLED,AUTO};
+	Ball_collector ball_collector;
 	//10 *position switches:
 	int auto_select;//0-19
 	//Dials:
