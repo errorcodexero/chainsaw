9326d8f1a2e06baeec674c7165c478820010512d
Adrian-Chimera <earthvexingtimelord@gmail.com>
2017-02-18 18:51:03 -0800

    Added more automodes into delat.cpp

--- executive/delay.cpp
+++ executive/delay.cpp
@@ -40,18 +40,18 @@ Executive auto_mode_convert(Next_mode_info info){
 
 	if (info.panel.in_use) {
 		switch(info.panel.auto_select){ 
-			case 1: 
+			case 1: //Do Nothing
 				return auto_null;
-			case 2:
+			case 2: //Baseline
 				//just go across the baseline
 				return auto_baseline;
-			case 3:
+			case 3: //Baseline Extended
 				//go across the baseline, then do another striaght drive.
 				return Executive{Chain{
 					Step{Drive_straight{12*12}},
 					dash
 				}};
-			case 4:
+			case 4: //Gear Boiler
 				return Executive{Chain{
 					Step{Drive_straight{5*12}},
 					Executive{Chain{
@@ -60,21 +60,63 @@ Executive auto_mode_convert(Next_mode_info info){
 							Step{Drive_straight{12}}, //approach
 							Executive{Chain{
 								Step{Geardrop()},
-								Executive{Teleop{}}
+								Executive{Chain{
+									Step{Drive_straight{-12}},	
+									Executive{Teleop{}}
+								}}
 							}}
 						}}
 					}}
 				}};
 			//	return Executive{Auto_gearboiler_topeg()};
-			case 5:
+			case 5: //Gear Boiler Extended
+				return Executive{Chain{
+					Step{Drive_straight{5*12}},
+					Executive{Chain{
+						Step{Turn{deg_to_rad(40)}},
+						Executive{Chain{
+							Step{Drive_straight{12}}, //approach
+							Executive{Chain{
+								Step{Geardrop()},
+								Executive{Chain{
+									Step{Drive_straight{-12}},
+									Executive{Chain{
+										Step{Turn{deg_to_rad(-40)}},
+										Executive{Chain{
+											Step{Drive_straight{4*12}},
+											Executive{Teleop{}}
+										}}
+									}}
+								}}
+							}}
+						}}
+					}}
+				}};
+
 			//	return Executive{Auto_gearboiler_topeg()};
-			case 6:
+			case 6: //Gear Mid
 			//	return Executive{Auto_gearboiler_topeg()};
 			case 7:
 				//return Executive{Auto_gearloading_topeg({0,0})};
 			case 8:
 				//return Executive{Auto_gearmid_topeg({0,0})};
-			case 9:
+			case 9: //Vision test (TEMP)
+				return Executive{Chain{
+					Step{Drive_straight{5*12}},
+					Executive{Chain{
+						Step{Turn{deg_to_rad(40)}},
+						Executive{Chain{
+							Step{Drive_straight{12}}, //approach
+							Executive{Chain{
+								Step{Geardrop()},
+								Executive{Chain{
+									Step{Drive_straight{-12}},
+									Executive{Teleop{}}
+								}}
+							}}
+						}}
+					}}
+				}};
 				//return Executive{Auto_gearmid_topeg({0,0})};
 			case 0:
 			default:
