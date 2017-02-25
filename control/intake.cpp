00bd9b2ae5cd05f476f6ab81bf8ee0094fc91907
Software3 <1425sw@gmail.com>
2017-02-25 09:52:09 -0800

    Match ball collector outputs with robot wiring

--- control/intake.cpp
+++ control/intake.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 #define INTAKE_ADDRESS 7
-#define INTAKE_SPEED 1.0
+#define INTAKE_SPEED .40 //TODO tune this
 
 ostream& operator<<(ostream& o, Intake::Goal a){
 	#define X(name) if(a==Intake::Goal::name)return o<<"Intake::Goal("#name")";
@@ -24,13 +24,13 @@ bool operator!=(Intake a, Intake b){ return !(a==b);}
 Robot_outputs Intake::Output_applicator::operator()(Robot_outputs r, Intake::Output out)const{
 	switch(out){
 		case Intake::Output::OUT:
-			r.pwm[INTAKE_ADDRESS]=-INTAKE_SPEED;
+			r.pwm[INTAKE_ADDRESS]=INTAKE_SPEED;
 			break;
 		case Intake::Output::OFF:
 			r.pwm[INTAKE_ADDRESS]=0.0;	
 			break;
 		case Intake::Output::IN:
-			r.pwm[INTAKE_ADDRESS]=INTAKE_SPEED;
+			r.pwm[INTAKE_ADDRESS]=-INTAKE_SPEED;
 			break;
 		default:
 			assert(0);
@@ -40,9 +40,9 @@ Robot_outputs Intake::Output_applicator::operator()(Robot_outputs r, Intake::Out
 
 Intake::Output Intake::Output_applicator::operator()(Robot_outputs r)const{
 	double pwm = r.pwm[INTAKE_ADDRESS];
-	if(pwm < 0.0) return Intake::Output::OUT;
+	if(pwm < 0.0) return Intake::Output::IN;
 	if(pwm == 0.0) return Intake::Output::OFF;
-	if(pwm > 0.0) return Intake::Output::IN;
+	if(pwm > 0.0) return Intake::Output::OUT;
 	assert(0);
 }
 	
