ef5059d8c713409e1522f14adf60359a088905db
Logan <logan.traffas@gmail.com>
2017-03-02 22:24:52 -0800

    Take advantage of Maybe in Bound_tracker

--- util/bound_tracker.cpp
+++ util/bound_tracker.cpp
@@ -6,11 +6,14 @@ using namespace std;
 
 #ifdef BOUND_TRACKER_TEST
 int main(){
-	Bound_tracker<double> b = {};
+	Bound_tracker<double> b;
+	cout<<b;
 	cout<<b<<"\n";
 	b.update(4);
 	cout<<b<<"\n";
 	b.update(2);
+	cout<<b<<"\n";
+	b.update(6);
 	cout<<b<<"\n";
 }
 #endif
--- util/bound_tracker.h
+++ util/bound_tracker.h
@@ -3,11 +3,40 @@
 
 #include <iosfwd>
 #include <iostream>
-
-//this would be easy to make a template on the value type.
+#include "maybe.h"
 
 template<typename T>
-struct Bound_tracker{//TODO: make variables private
+struct Bound_tracker{
+	struct Bounds{
+		T min, max;
+		Bounds(T a, T b):min(a),max(b){}
+
+		friend std::ostream& operator<<(std::ostream& o, Bounds a){
+			o<<"min:"<<a.min;
+			o<<" max:"<<a.max;
+			return o;
+		}		
+
+	};
+	
+	Maybe<Bounds> bounds;
+	bool data;
+	
+	Bound_tracker():data(false){}
+	
+	void update(T const& v){
+		if(data){
+			//std::min and std::max don't work right on the cRIO. //TODO see if this works on the roboRIO
+			//min=std::min(min,v);
+			//max=std::max(max,v);
+			if(v < (*bounds).min) (*bounds).min=v;
+			if(v > (*bounds).max) (*bounds).max=v;
+		}else{
+			bounds = {v,v};
+			data = true;
+		}
+	}
+	/*
 	T min,max;
 	bool data;
 	
@@ -29,19 +58,15 @@ struct Bound_tracker{//TODO: make variables private
 			data= true;
 		}
 	}
-
+	*/
 	//friend std::ostream& operator<<(std::ostream&,Bound_tracker<T>){
 	
 };
 
 template<typename T>
-std::ostream& operator<<(std::ostream& o,Bound_tracker<T> a){
+std::ostream& operator<<(std::ostream& o,const Bound_tracker<T> a){
 	o<<"Bound_tracker(";
-	if(a.data){
-		o<<"min:"<<a.min<<" max:"<<a.max;
-	}else{
-		o<<"Not Set";
-	}
+	o<<a.bounds;
 	return o<<")";
 }
 
